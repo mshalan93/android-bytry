badb7001169d33894aa25098285152deb5ffb217
mshalan93 <msh3lan93@gmail.com>
2020-08-19 23:51:05 +0200

    add setting activity

--- app/src/main/java/com/example/bytar/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/bytar/ui/home/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.bytar.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bytar.R
+import com.example.bytar.ui.Setting.SettingActivity
+import com.example.bytar.ui.auth.LoginActivity
 import com.example.bytar.ui.homeScreen.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +27,25 @@ class MainActivity : AppCompatActivity() {
                 .commit()
         }
 
+    }
+
+    // create an action bar button
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
+        // If you don't have res/menu, just create a directory named "menu" inside res
+        menuInflater.inflate(R.menu.setting_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // setting button activities
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id: Int=item.getItemId()
+        if (id == R.id.mybutton) {
+            // do something here
+            val nextScreenIntent =Intent(this, SettingActivity::class.java)
+            startActivity(nextScreenIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
         when (menuItem.itemId) {
